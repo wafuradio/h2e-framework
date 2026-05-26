8f74ea38547434e76ebc2e834e42b1380db72778
wafuradio <wafuradio@hotmail.com>
2026-05-26 13:53:27 -0400

    update astro coinfig file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   }
