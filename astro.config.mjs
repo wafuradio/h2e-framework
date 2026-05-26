67a73a7e1a6e677df27569e4699fcf9acfd90255
wafuradio <wafuradio@hotmail.com>
2026-05-26 13:58:37 -0400

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
