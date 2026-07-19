c98f28bbd7a0012b491aad7dcae8a41e1f3683b3
wafuradio <wafuradio@hotmail.com>
2026-07-19 17:26:46 -0400

    chaplet2 audio, new images, manuscript tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
-
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: netlify()
+  }
 });
